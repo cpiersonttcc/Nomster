7d08a696e7bf8cdbdeb9ffb34c74c22ae31257df
cpiersonttcc <cpierson@trusted-name.com>
2015-11-24 16:08:43 -0500

    previewing email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,18 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: 'christopher.r.pierson@gmail.com',
+    password: 'Piech01!!@%'
+  }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -14,7 +26,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Send messages from local machine
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
