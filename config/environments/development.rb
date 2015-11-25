743e55d39512b2d574434a8ed651698fd097c476
cpiersonttcc <cpierson@trusted-name.com>
2015-11-25 10:45:01 -0500

    adding metrics to user profile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,17 +2,17 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
 
-  # config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
 
-  # config.action_mailer.smtp_settings = {
-  #   address: "smtp.gmail.com",
-  #   port: 587,
-  #   domain: "example.com",
-  #   authentication: "plain",
-  #   enable_starttls_auto: true,
-  #   user_name: 'christopher.r.pierson@gmail.com',
-  #   password: 'Piech01!!@%'
-  # }
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: 'christopher.r.pierson@gmail.com',
+    password: 'Piech01!!@%'
+  }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
